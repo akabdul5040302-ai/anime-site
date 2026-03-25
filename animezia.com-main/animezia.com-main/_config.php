abc20b8e6caedbf3154183f2ec76a56acc46cb7a
akabdul5040302-ai <ak.abdul5040302@gmail.com>
2026-03-26 03:31:35 +0600

    Add files via upload

---
+++ animezia.com-main/animezia.com-main/_config.php
@@ -0,0 +1,32 @@
+<?php 
+
+$websiteTitle = "AnimeZia"; // Your Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = "https://cdnzia.pages.dev/images/logo.webp"; // Your Logo
+$websiteLogo2 = "https://cdnzia.pages.dev/images/logo2.webp";
+$contactEmail = ""; // Contact Email, put your own.
+
+
+//Donate 
+$donate = "https://coindrop.to/animezia";
+
+// Socials 
+$telegram = "https://telegram.me/anime_arsenal"; // telegram
+$discord = "#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+// *** IMPORTANT ***  
+// remove last slash - "/" from links if present.  
+
+$disqus = "https://watchzia.disqus.com"; // Disqus make your own
+
+$cdn = "https://cdnzia.pages.dev"; // cdn repo https://github.com/warlordsnet/cdnzia
+
+// API URL
+$api ="https://api.animezia.com"; // api repo https://github.com/warlordsnet/zia-api - Replace with your actual API URL
+
+$imgk = ""; // your imagekit id, make your own. instructions in readme.
+
+$banner = "https://cdnzia.pages.dev/images/banner.webp";  //Banner use your own
+?>
